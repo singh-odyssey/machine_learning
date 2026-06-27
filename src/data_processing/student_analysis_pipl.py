c82d0583a5129bca37e7d793ede2bfa79947f1c5
Aditya Singh <148507838+singh-odyssey@users.noreply.github.com>
2026-06-27 19:36:03 +0000

    Add ml_pipeline_student_analysis

---
+++ src/data_processing/student_analysis_pipl.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+
+numeric_features = [
+    "Hours_Studied",
+    "Attendance",
+    "Previous_Scores", # Assuming this is "Exam Marks" or the feature you want to cap
+    "Tutoring_Sessions",
+]
+categorical_features = ["Teacher_Quality", "Distance_from_Home", "Parental_Involvement"]
+
+# Corrected numeric_transformer (this pipeline includes the capper for now)
+numeric_transformer = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("capper_max", FunctionTransformer(func=lambda X: np.clip(X,a_min=0, a_max=99), validate=False)),
+        ("scaler", StandardScaler())
+    ]
+)
+
+categorical_transformer=Pipeline(
+    steps=[
+        ("imputer"),SimpleImputer(strategy="most_frequent"),
+        ("onehot",OneHotEncoder(handle_unknown='ignore',sparse_output=False))
+    ]) 
+
+preprocessor=ColumnTransformer(
+    transformer=[
+        ('num',numeric_transformer,numeric_features),
+        ('cat',categorical_transformer,categorical_features)
+    ]
+)
+
