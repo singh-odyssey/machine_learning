ef98452a45638a79c41478350a52fa63a94c9be1
singh-odyssey <148507838+singh-odyssey@users.noreply.github.com>
2026-07-27 06:50:22 +0000

    Model: Add cali_house_MLP_v1

---
+++ src/deep_learning_models/general/cali_housing_MLP_v1.py
@@ -0,0 +1,40 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.src.models import Sequential
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from src.utils.supervised_metrics import evaluate_regression
+from src.utils.tracker import log_experiment
+
+
+housing_data = fetch_california_housing(as_frame=True)
+print(housing_data)
+
+X = housing_data.data
+y = housing_data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+scale = StandardScaler()
+X_train_scaled = scale.fit_transform(X_train)
+X_test_scaled = scale.transform(X_test)
+
+model = Sequential()
+input_dim = X_train.shape[1]
+model.add(Dense(128, activation="relu", input_dim=input_dim))
+model.add( Dense( 64,activation="relu"))
+model.add(Dense(32, activation="relu"))
+model.add(Dense(16, activation="relu"))
+model.add(Dense(1, activation="linear"))
+
+model.compile(optimizer="adam", loss="mse", metrics=["mae"])
+
+model.fit(X_train_scaled, y_train, epochs=50, validation_split=0.2 )
+prediction = model.predict(X_test_scaled)
+
+score = evaluate_regression(y_test, prediction)
+log_experiment(model, score, "cali_housing_MLP_v1")
